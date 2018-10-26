339980eec9066eb203b724ddfd3fbac0f3c3da96
vinaytiwari <vinaytiwari264@gmail.com>
2018-10-26 03:15:45 -0700

    Added  Product and Caategory  Business Entities and Created Database

---
+++ ClothBajar.DataBase/CBContext.cs
@@ -0,0 +1,21 @@
+﻿using ClothBajar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBajar.DataBase
+{
+    public class CBContext : DbContext
+    {
+        public CBContext() : base("ClothBajarConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ClothBajar.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBajar.Entities
+{
+    public class Product : BaseEntity
+    {
+        public decimal Price { get; set; }
+
+        public Category Category { get; set; }
+
+        
+    }
+}
